df6f1b62aafca7302a39bcc5c623ed95f827ede4
ayano-i3 <ayano.o@i3-systems.com>
2023-11-03 17:57:13 +0900

    first commit

---
+++ stopWatch/stopWatch/stopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  stopWatchApp.swift
+//  stopWatch
+//
+//  Created by 奥田彩乃 on 2023/11/03.
+//
+
+import SwiftUI
+
+@main
+struct stopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
