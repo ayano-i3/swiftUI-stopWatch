3503a9e37717073fdea6fa037b678452643ce413
ayano-i3 <ayano.o@i3-systems.com>
2023-11-06 19:53:47 +0900

    add animation

--- stopWatch/stopWatch/CongratulationsView.swift
+++ stopWatch/stopWatch/CongratulationsView.swift
@@ -8,21 +8,84 @@
 import SwiftUI
 
 struct CongratulationsView: View {
+    @State private var fallingImage = false
+    @State private var LPosition = -400.0
+    @State private var RPosition = -150.0
+    @State private var centerPosition = -50.0
+    
+    let animation = Animation
+         .linear(duration: 3.0)
+         .repeatForever(autoreverses: false)
+    
+    
     var body: some View {
         VStack {
-        
-            Image("Congraturation_icon_image")
-                .resizable()
-                .frame(width: 200, height: 200)
-
-
+            ZStack{
+                //TODO: ClapImageが表を通ってほしい
+                HStack {
+                    
+                    Spacer()
+                    
+                    Image("Clap_image")
+                        .resizable()
+                        .frame(width: 90, height: 90)
+                    
+                        .offset(y: fallingImage ? UIScreen.main.bounds.height + 300 : LPosition)
+                        .animation(fallingImage ? animation : .default, value: fallingImage)
+                        .onAppear {
+                            fallingImage = true
+                        }
+                    
+                    
+                    Spacer()
+                    
+                    Image("Clap_image")
+                        .resizable()
+                        .frame(width: 90, height: 90)
+                    
+                        .offset(y: fallingImage ? UIScreen.main.bounds.height + 300 : centerPosition)
+                        .animation(fallingImage ? animation : .default, value: fallingImage)
+                        .onAppear {
+                            fallingImage = true
+                        }
+                    
+                    
+                    Spacer()
+                    
+                    Image("Clap_image")
+                        .resizable()
+                        .frame(width: 90, height: 90)
+                    
+                        .offset(y: fallingImage ? UIScreen.main.bounds.height + 300 : RPosition)
+                        .animation(fallingImage ? animation : .default, value: fallingImage)
+                        .onAppear {
+                            fallingImage = true
+                        }
+                    
+                    
+                    Spacer()
+                }
+                
+                
+            }
             
-            Text("10.00")
-                .font(.system(size: 100))
-                .bold()
+
+                VStack {
+                    
+                    Image("Congraturation_icon_image")
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                    
+                    
+                    
+                    Text("10.00")
+                        .font(.system(size: 100))
+                        .bold()
+                    
+                }
                 .padding(.bottom, 200)
+            }.padding(.bottom, 200)
         }
-    }
 }
 
 struct CongratulationsView_Previews: PreviewProvider {
